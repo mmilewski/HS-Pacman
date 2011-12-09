782d3b134c8da6e096cd55ad479acb77a3905ad0
Marcin Milewski <mmilewski@gmail.com>
2011-12-09 19:24:16 +0100

    extracted function which loads images

--- Main.hs
+++ Main.hs
@@ -12,16 +12,28 @@ import System.Random
 
 import System.Posix as P  -- usleep
 
+import Data.List as List
+import Data.Map as Map
+
+img_smile = "smile"
 
 (width, height) = (800, 600)
+
+
+loadImages = let ext = ".bmp"
+                 loadImg name = loadBMP $ name ++ ext
+             in do image <- loadImg $ img_smile
+                   return $ Map.fromList [ (img_smile, image)
+                                         ]
 
 main = withInit [InitVideo] $
     do screen <- setVideoMode width height 16 [SWSurface]
        setCaption "Test" ""
        enableUnicode True
-       image <- loadBMP "image.bmp"
-       display image
-       loop (display image)
+       images <- loadImages
+       let image = images Map.! img_smile in
+         do display image
+            loop (display image)
 
 display :: Surface -> IO ()
 display image
