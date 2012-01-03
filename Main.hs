27993e99dd38eaa6eb284fa8cd4b9b723401c499
Marcin Milewski <mmilewski@gmail.com>
2012-01-03 17:24:41 +0100

    cherry-pick from branch move-till-wall

--- Main.hs
+++ Main.hs
@@ -10,77 +10,81 @@ import Data.Map (Map, fromList, lookup)
 import Data.Maybe (fromJust)
 import Data.Bits ((.&.))
 
-type Object = Vector
-type Objects = [Object]
-type Screen = Surface
 type SurfacesMap = Map String Surface
 type TimeDelta = Float
 type CpuTime = Integer
+type Screen = Surface
 
-(window_width, window_height) = (800, 600)
-boardSize = (16, 12)   -- width, height
 
-img_placeholder = "smile"
-img_pacman = "pacman"
+(windowWidth, windowHeight) = (800, 600)       -- rozmiar okna w px
+(boardWidth, boardHeight) = (16, 12)           -- ilosc kafli w poziomie i pionie
+boardSize = (boardWidth, boardHeight)
+(tileW, tileH) = (windowWidth `div` boardWidth, windowHeight `div` boardHeight)
+
+img_placeholder = img_smile
+img_smile = "smile"
+img_pacman = img_smile --"pacman"
 img_board_empty = "board-empty"
 img_board_bottom = "board-bottom"
 img_board_right = "board-right"
 img_board_left = "board-left"
 img_board_top = "board-top"
-img_all = [img_placeholder, img_pacman, img_board_empty, img_board_bottom, img_board_right, img_board_left, img_board_top]
+img_all = [img_smile, img_pacman, img_board_empty, img_board_bottom, img_board_right, img_board_left, img_board_top]
+
+roundFromIntegral x = round $ fromIntegral x
 
 loadImages :: IO (SurfacesMap)
 loadImages
     = do surfaces <- mapM (\name -> Image.load $ concat ["gfx/", name, ".png"]) img_all
          return $ fromList $ zip img_all surfaces
 
-displayObject :: Screen -> Vector -> Surface -> IO ()
-displayObject screen (Vector x y) image
-    = do blitSurface image Nothing screen (Just $ Rect (round x) (round y) 0 0 )
-         return ()
-
-displayBoardPiece :: Surface -> SurfacesMap -> (Int, Int) -> (Int, Int) -> IO ()
-displayBoardPiece screen imagesMap (boardWidth, boardHeight) (i, piece)
-    = do blit $ surfaceByPiece (piece .&. 1)
-         blit $ surfaceByPiece (piece .&. 2)
-         blit $ surfaceByPiece (piece .&. 4)
-         blit $ surfaceByPiece (piece .&. 8)
-         where blit surface = blitSurface surface Nothing screen (Just $ Rect (round $ tileW * col) (round $ row * tileH) 0 0) >> return ()
-               surfaceByPiece p = fromJust $ lookup (fromJust $ lookup p boardMap) imagesMap
-               boardMap = fromList [(0, img_board_empty), (1, img_board_top), (2, img_board_right), (4, img_board_bottom), (8, img_board_left)]
-               (tileW, tileH) = (50, 50)
-               row = fromIntegral $ i `div` boardWidth
-               col = fromIntegral $ i `mod` boardWidth
-
 display :: GameData -> SurfacesMap -> IO ()
-display (GameData objects (Player pos) board) imagesMap =
-  do screen <- getVideoSurface
-     green <- mapRGB (surfaceGetPixelFormat screen) 0 0xFF 0
-     fillRect screen Nothing green
-     mapM_ (\ip -> displayBoardPiece screen imagesMap boardSize ip) $ zip (List.iterate (+1) 0) board
-     let placeholder = fromJust $ lookup img_placeholder imagesMap
-     mapM_ (\objPos -> displayObject screen objPos placeholder) objects
-     displayObject screen pos (fromJust $ lookup img_pacman imagesMap)
-     SDL.flip screen
+display (GameData objects (Player plPos) board) imagesMap
+    = do screen <- getVideoSurface
+         displayBg screen
+         displayBoard screen
+         displayObjects screen
+         displayPlayer screen
+         SDL.flip screen
+      where
+        displayBg screen = mapRGB (surfaceGetPixelFormat screen) 0x1F 0x1F 0x1F >>= (\grey -> fillRect screen Nothing grey)
+        displayBoard screen = mapM_ (\i_p -> displayBoardPiece screen imagesMap boardSize i_p) $ zip (iterate (+1) 0) board
+        displayObjects screen = do let placeholder = fromJust $ lookup img_placeholder imagesMap
+                                   mapM_ (\objPos -> displayObject screen objPos placeholder) objects
+        displayPlayer screen = displayObject screen plPos (fromJust $ lookup img_pacman imagesMap)
 
-moveObjects :: Objects -> TimeDelta -> Objects
-moveObjects objects dt
-    = map move objects
-      where move (Vector x y) = Vector x' y
-              where x' = if x > fromIntegral window_width then 0 else x + 20.0 * dt
+        displayObject :: Screen -> Position -> Surface -> IO ()
+        displayObject screen (Vector x y) image = do blitSurface image Nothing screen (Just $ Rect (round x) (round y) 0 0 ) >> return ()
+        displayBoardPiece :: Surface -> SurfacesMap -> (Int, Int) -> (Int, Int) -> IO ()
+        displayBoardPiece screen imagesMap (boardWidth, boardHeight) (i, piece)
+            = do blit $ surfaceByPiece (piece .&. 1)
+                 blit $ surfaceByPiece (piece .&. 2)
+                 blit $ surfaceByPiece (piece .&. 4)
+                 blit $ surfaceByPiece (piece .&. 8)
+                 where blit surface = blitSurface surface Nothing screen (Just $ Rect (tileW * col) (tileH * row) 0 0) >> return ()
+                       surfaceByPiece p = fromJust $ lookup (fromJust $ lookup p boardMap) imagesMap
+                       boardMap = fromList [(0, img_board_empty), (1, img_board_top), (2, img_board_right), (4, img_board_bottom), (8, img_board_left)]
+                       row = fromIntegral $ i `div` boardWidth
+                       col = fromIntegral $ i `mod` boardWidth
 
 data Vector = Vector Float Float
+type Position = Vector
 vadd (Vector a b) (Vector c d) = Vector (a + c) (b + d)
 vscale (Vector a b) factor = Vector (a * factor) (b * factor)
 instance Show Vector where
     show (Vector a b) = "[" ++ (show a) ++ ", " ++ (show b) ++ "]"
 
+type Object = Vector
+type Objects = [Object]
+
+moveObjects :: Objects -> TimeDelta -> Objects
+moveObjects objects dt = map move objects
+    where move (Vector x y) = Vector x' y where x' = if round x >= windowWidth then 0 else x + 20.0 * dt
 
 data Player = Player { pos :: Vector
                      } deriving (Show)
 
-movePlayer :: Player -> Vector -> Player
-movePlayer (Player pos) pos' = Player (vadd pos pos')
+
 
 type Board = [Int]
 data GameData = GameData { objects :: Objects,
@@ -88,15 +92,32 @@ data GameData = GameData { objects :: Objects,
                            board :: Board
                          } deriving (Show)
 
+{-
+posx' <- posx + velx * dt
+
+w lewo
+   można iść dopóki  brick(posx', posy) == brick(posx, posy)
+
+w prawo
+   można iść dopóki  brick(posx'+tileW, posy) == brick(posx+tileW, posy)
+-}
+brickAt :: Board -> Int -> Int -> Int
+brickAt board x y = List.head $ List.drop n board
+    where n = (roundFromIntegral$ y `div` tileH) * boardWidth + (roundFromIntegral$ x `div` tileW)
+
+movePlayer :: Player -> Vector -> Player
+movePlayer (Player pos) pos' = Player (vadd pos pos')
+
 handleEvent :: TimeDelta -> Event -> GameData -> IO(GameData)
 handleEvent dt SDL.NoEvent gd = return gd
 handleEvent dt SDL.Quit gd = exitWith ExitSuccess
 handleEvent dt (KeyDown (Keysym _ _ 'q')) gd = exitWith ExitSuccess
-handleEvent dt (KeyDown keysym) (GameData objs pacman board) = handleKeyDown keysym where
+handleEvent dt (KeyDown keysym) gd@(GameData objs pacman board) = handleKeyDown keysym where
     handleKeyDown (Keysym SDLK_RIGHT _ _) = return $ GameData objs (movePlayer pacman (Vector ( 40*dt) 0)) board
     handleKeyDown (Keysym SDLK_LEFT  _ _) = return $ GameData objs (movePlayer pacman (Vector (-40*dt) 0)) board
     handleKeyDown (Keysym SDLK_DOWN  _ _) = return $ GameData objs (movePlayer pacman (Vector 0 ( 40*dt))) board
     handleKeyDown (Keysym SDLK_UP    _ _) = return $ GameData objs (movePlayer pacman (Vector 0 (-40*dt))) board
+    handleKeyDown _                       = return gd
 handleEvent _ _ gd = return gd
 
 loop :: CpuTime -> GameData -> SurfacesMap -> IO ()
@@ -112,8 +133,23 @@ loop startTime gameData images
          display gameData' images
          loop endTime gameData' images
 
+assert :: Show a => Eq a => a -> a -> String -> IO()
+assert expected actual msg = if expected == actual then return () else error $ concat ["\nexpected: ", (show expected), "\nactual: ", (show actual), "\ndata: ", msg]
+
 main = withInit [InitVideo] $
-    do screen <- setVideoMode window_width window_height 16 [SWSurface]
+    do screen <- setVideoMode windowWidth windowHeight 16 [SWSurface]
+
+       let nums = iterate (+1) 0
+       mapM_ (\(x,y) -> assert 0 (brickAt nums x y) (show x ++ " , " ++ show y)) $ zip (take 50 $ drop (0*50) nums) (take 50 nums)
+       mapM_ (\(x,y) -> assert 1 (brickAt nums x y) (show x ++ " , " ++ show y)) $ zip (take 50 $ drop (1*50) nums) (take 50 nums)
+       mapM_ (\(x,y) -> assert 5 (brickAt nums x y) (show x ++ " , " ++ show y)) $ zip (take 50 $ drop (5*50) nums) (take 50 nums)
+       mapM_ (\(x,y) -> assert (0+1*16) (brickAt nums x y) (show x ++ " , " ++ show y)) $ zip (take 50 $ drop (0*50) nums) (take 50 $ drop(1*50) $ nums)
+       mapM_ (\(x,y) -> assert (1+1*16) (brickAt nums x y) (show x ++ " , " ++ show y)) $ zip (take 50 $ drop (1*50) nums) (take 50 $ drop(1*50) $ nums)
+       mapM_ (\(x,y) -> assert (5+1*16) (brickAt nums x y) (show x ++ " , " ++ show y)) $ zip (take 50 $ drop (5*50) nums) (take 50 $ drop(1*50) $ nums)
+       mapM_ (\(x,y) -> assert (0+10*16) (brickAt nums x y) (show x ++ " , " ++ show y)) $ zip (take 50 $ drop (0*50) nums) (take 50 $ drop(10*50) $ nums)
+       mapM_ (\(x,y) -> assert (1+10*16) (brickAt nums x y) (show x ++ " , " ++ show y)) $ zip (take 50 $ drop (1*50) nums) (take 50 $ drop(10*50) $ nums)
+       mapM_ (\(x,y) -> assert (5+10*16) (brickAt nums x y) (show x ++ " , " ++ show y)) $ zip (take 50 $ drop (5*50) nums) (take 50 $ drop(10*50) $ nums)
+
        setCaption "HS-Pacman" ""
        enableUnicode True
        images <- loadImages
