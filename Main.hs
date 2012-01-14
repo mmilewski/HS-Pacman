8e82ee72fe625383352e4d29567094d89a2a56ff
Marcin Milewski <mmilewski@gmail.com>
2012-01-14 15:37:09 +0100

    refactore

--- Main.hs
+++ Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Prelude hiding (lookup)
-import Graphics.UI.SDL as SDL
+import Graphics.UI.SDL as SDL hiding (update)
 import Graphics.UI.SDL.Image as Image
 import System.Exit
 import System.CPUTime
@@ -16,13 +16,10 @@ type TimeDelta = Float
 type CpuTime = Integer
 type Screen = Surface
 
-
 (windowWidth, windowHeight) = (800, 600)       -- rozmiar okna w px
 (boardWidth, boardHeight) = (16, 12)           -- ilosc kafli w poziomie i pionie
 boardSize = (boardWidth, boardHeight)
 (tileW, tileH) = (windowWidth `div` boardWidth, windowHeight `div` boardHeight)
-
-plSpeed = 16         -- player/pacman's speed (px/s)
 
 img_placeholder = img_smile
 img_smile = "smile"
@@ -39,6 +36,7 @@ img_all = [img_ball, img_smile, img_enemy, img_pacman, img_board_empty, img_boar
 float :: Int -> Float      -- konwersja Int ~~> Float
 float = fromIntegral
 
+mignore :: Monad m => m a -> m ()
 mignore a = a >> return ()
 
 loadImages :: IO (SurfacesMap)
@@ -46,26 +44,7 @@ loadImages
     = do surfaces <- mapM (\name -> Image.load $ concat ["gfx/", name, ".png"]) img_all
          return $ fromList $ zip img_all surfaces
 
-display :: GameData -> SurfacesMap -> IO ()
-display (GameData objects balls (Player plPos _) enemies board) imagesMap
-    = getVideoSurface >>= (\screen -> displayAt screen >> SDL.flip screen)
-      where
-        displayAt screen = displayBg >> displayBoard >> displayBalls >> displayEnemies >> displayObjects >> displayPlayer
-          where
-            displayBg      = mapRGB (surfaceGetPixelFormat screen) 0x1F 0x1F 0x1F >>= (\grey -> fillRect screen Nothing grey)
-            displayBoard   = mapM_ (\i_p -> displayBoardPiece imagesMap i_p) $ zip (iterate (+1) 0) board
-            displayBalls   = mapM_ (displayImage img_ball) balls
-            displayEnemies = mapM_ (displayImage img_enemy) enemies
-            displayObjects = mapM_ (displayImage img_enemy) objects
-            displayPlayer  =       (displayImage img_pacman) plPos
-            displayImage imgName (Vector x y) = mignore$ blitSurface (surfaceByName imgName) Nothing screen (Just $ Rect (round x) (round y) 0 0 )
-            surfaceByName imgName = fromJust $ lookup imgName imagesMap
-            displayBoardPiece imagesMap (i, piece) = mapM_ blit [(piece .&. 1), (piece .&. 2), (piece .&. 4), (piece .&. 8)]
-                     where blit p = mignore$ blitSurface (surfaceByName (nameOf p)) Nothing screen (Just $ Rect (tileW * col) (tileH * row) 0 0)
-                           nameOf p = fromJust $ lookup p $ fromList [(0, img_board_empty), (1, img_board_top), (2, img_board_right),
-                                                                      (4, img_board_bottom), (8, img_board_left)]
-                           (row, col) = divMod i boardWidth
-
+---- Vector
 data Vector = Vector Float Float
 type Position = Vector
 vadd (Vector a b) (Vector c d) = Vector (a + c) (b + d)
@@ -75,62 +54,42 @@ vscale (Vector a b) factor = Vector (a * factor) (b * factor)
 instance Show Vector where
     show (Vector a b) = "[" ++ (show a) ++ ", " ++ (show b) ++ "]"
 
-type Object = Vector
-type Objects = [Object]
-type Balls = Objects
-type Enemy = Vector
-type Enemies = [Enemy]
-
-makeEnemy (Vector x y) = Vector x y
-
-moveObjects :: Objects -> TimeDelta -> Objects
-moveObjects objects dt = map move objects
-    where move (Vector x y) = Vector x' y where x' = if round x >= windowWidth then 0 else x + 20.0 * dt
-
-data Direction = N | W | S | E | X deriving (Show, Eq)
-
-data Player = Player { pos :: Vector,
-                       dir :: Direction
-                     } deriving (Show)
-
-getPlPos :: Player -> Vector
-getPlPos Player{pos=p} = p
-
+---- Board
 type Board = [Int]
 
 brickAt :: Board -> Int -> Int -> Int
 brickAt board col row = board !! ((fromIntegral$ row `div` tileH) * boardWidth + (fromIntegral$ col `div` tileW))
 
-data GameData = GameData { objects :: Objects,
-                           balls :: Balls,
-                           pacman :: Player,
-                           enemies :: Enemies,
-                           board :: Board
-                         } deriving (Show)
+---- Ball
+type Ball = Vector
+type Balls = [Ball]
 
--- -- tak napisałbym w każdym sensownym języku. Niestety Haskell nie ogarnia sytuacji
--- -- i zmusił mnie do zrobienia brzydkiego obejścia, bleh :(
--- getNearestCenter plPos@(Vector x y) = Vector x' y' where x' = (floor$ x/tileW) * tileW + tileW/2
---                                                          y' = (floor$ y/tileH) * tileH + tileH/2
-getNearestCenter (Vector x y) = Vector (findCoordX x) (findCoordY y)
-findCoordX :: Float -> Float
-findCoordX x = if 0 <= x && x < (float tileW) then (float tileW)/2 else (float tileW) + findCoordX (x - float tileW)
-findCoordY :: Float -> Float
-findCoordY x = if 0 <= x && x < (float tileH) then (float tileH)/2 else (float tileH) + findCoordY (x - float tileH)
+makeBall :: Vector -> Ball
+makeBall pos = pos
 
-changeDir :: Player -> TimeDelta -> Direction -> Board -> Player
-changeDir pacman@(Player plPos@(Vector px py) plDir) dt newDir _
-    = if plDir == newDir then pacman
-      else if (plDir, newDir) `elem` [(N,S), (S,N), (W,E), (E,W)] || newDir == X then pacman {dir = newDir}
-      else if plDir `elem` [E, W] && 15 < (abs $ cx - px) then pacman
-      else if plDir `elem` [S, N] && 15 < (abs $ cy - py) then pacman
-      else pacman {dir = newDir, pos = plPos'}
-           where plPos' = centeredPos
-                 centeredPos@(Vector cx cy) = getNearestCenter (plPos `vadd` tileHalf) `vsub` tileHalf
-                 tileHalf = Vector (float tileW) (float tileH) `vscale` 0.5
+---- Enemy
+type Enemy = Vector
+type Enemies = [Enemy]
 
-movePacman :: TimeDelta -> Player -> Board -> Player
-movePacman dt pacman@(Player pos@(Vector px py) dir) board
+makeEnemy :: Vector -> Ball
+makeEnemy pos = pos
+
+---- Player
+data Direction = N | W | S | E | X deriving (Show, Eq)
+data Player = Player { pos :: Vector,
+                       dir :: Direction
+                     } deriving (Show)
+
+plSpeed = 16 :: Float    -- player/pacman's speed (px/s)
+
+makePlayer :: Vector -> Player
+makePlayer pos = Player pos X
+
+plGetPos :: Player -> Vector
+plGetPos Player{pos=p} = p
+
+plMove :: Player -> TimeDelta -> Board -> Player
+plMove pacman@(Player pos@(Vector px py) dir) dt board
     = pacman{pos=pos `vadd` Vector dx dy}
       where
         dx = if dir `elem` [X, N, S] then 0
@@ -142,34 +101,82 @@ movePacman dt pacman@(Player pos@(Vector px py) dir) board
                       S -> if (brickAt board (round px) (        (round $ py + plSpeed*dt)) .&. 4 /= 0) then 0 else  plSpeed*dt
                       N -> if (brickAt board (round px) (tileH + (round $ py - plSpeed*dt)) .&. 1 /= 0) then 0 else -plSpeed*dt)
 
-handleEvent :: TimeDelta -> Event -> GameData -> IO(GameData)
-handleEvent dt SDL.NoEvent gd = return gd
-handleEvent dt SDL.Quit gd = exitWith ExitSuccess
-handleEvent dt (KeyDown (Keysym _ _ 'q')) gd = exitWith ExitSuccess
-handleEvent dt (KeyDown keysym) gd@(GameData _ _ pacman _ board) = handleKeyDown keysym where
-    updatePacmanDir dir = return $ gd{pacman = changeDir pacman dt dir board}
-    handleKeyDown (Keysym SDLK_RIGHT _ _) = updatePacmanDir E
-    handleKeyDown (Keysym SDLK_LEFT  _ _) = updatePacmanDir W
-    handleKeyDown (Keysym SDLK_DOWN  _ _) = updatePacmanDir S
-    handleKeyDown (Keysym SDLK_UP    _ _) = updatePacmanDir N
-    handleKeyDown _                       = return gd
-handleEvent _ _ gd = return gd
+plUpdateDir :: Player -> TimeDelta -> Direction -> Player
+plUpdateDir pacman@(Player plPos@(Vector px py) plDir) dt newDir
+    = if plDir == newDir then pacman
+      else if (plDir, newDir) `elem` [(N,S), (S,N), (W,E), (E,W)] || newDir == X then pacman {dir = newDir}
+      else if plDir `elem` [E, W] && 15 < (abs $ cx - px) then pacman
+      else if plDir `elem` [S, N] && 15 < (abs $ cy - py) then pacman
+      else pacman {dir = newDir, pos = plPos'}
+           where plPos' = centeredPos
+                 centeredPos@(Vector cx cy) = getNearestCenter (plPos `vadd` tileHalf) `vsub` tileHalf
+                 tileHalf = Vector (float tileW) (float tileH) `vscale` 0.5
+                 -- -- tak napisałbym w każdym sensownym języku. Niestety Haskell nie ogarnia sytuacji
+                 -- -- i zmusił mnie do zrobienia brzydkiego obejścia, bleh :(
+                 -- getNearestCenter plPos@(Vector x y) = Vector x' y' where x' = (floor$ x/tileW) * tileW + tileW/2
+                 --                                                          y' = (floor$ y/tileH) * tileH + tileH/2
+                 getNearestCenter (Vector x y) = Vector (findCoordX x) (findCoordY y)
+                   where findCoordX :: Float -> Float
+                         findCoordX x = if 0 <= x && x < (float tileW) then (float tileW)/2 else (float tileW) + findCoordX (x - float tileW)
+                         findCoordY :: Float -> Float
+                         findCoordY x = if 0 <= x && x < (float tileH) then (float tileH)/2 else (float tileH) + findCoordY (x - float tileH)
 
+---- GameData
+data GameData = GameData { balls :: Balls,
+                           pacman :: Player,
+                           enemies :: Enemies,
+                           board :: Board
+                         } deriving (Show)
+
+gdDisplay :: GameData -> SurfacesMap -> IO ()
+gdDisplay (GameData balls (Player plPos _) enemies board) imagesMap
+    = getVideoSurface >>= (\screen -> displayAt screen >> SDL.flip screen)
+      where
+        displayAt screen = displayBg >> displayBoard >> displayBalls >> displayEnemies >> displayPlayer
+          where
+            displayBg      = mapRGB (surfaceGetPixelFormat screen) 0x1F 0x1F 0x1F >>= (\grey -> fillRect screen Nothing grey)
+            displayBoard   = mapM_ (\i_p -> displayBoardPiece imagesMap i_p) $ zip (iterate (+1) 0) board
+            displayBalls   = mapM_ (displayImage img_ball) balls
+            displayEnemies = mapM_ (displayImage img_enemy) enemies
+            displayPlayer  =       (displayImage img_pacman) plPos
+            displayImage imgName (Vector x y) = mignore$ blitSurface (surfaceByName imgName) Nothing screen (Just $ Rect (round x) (round y) 0 0 )
+            surfaceByName imgName = fromJust $ lookup imgName imagesMap
+            displayBoardPiece imagesMap (i, piece) = mapM_ blit [(piece .&. 1), (piece .&. 2), (piece .&. 4), (piece .&. 8)]
+                     where blit p = mignore$ blitSurface (surfaceByName (nameOf p)) Nothing screen (Just $ Rect (tileW * col) (tileH * row) 0 0)
+                           nameOf p = fromJust $ lookup p $ fromList [(0, img_board_empty), (1, img_board_top), (2, img_board_right),
+                                                                      (4, img_board_bottom), (8, img_board_left)]
+                           (row, col) = divMod i boardWidth
+
+gdHandleEvent :: GameData -> TimeDelta -> Event -> IO(GameData)
+gdHandleEvent gd _ SDL.NoEvent = return gd
+gdHandleEvent gd _ SDL.Quit = exitWith ExitSuccess
+gdHandleEvent gd _ (KeyDown (Keysym _ _ 'q')) = exitWith ExitSuccess
+gdHandleEvent gd@(GameData _ pacman _ board) dt (KeyDown (Keysym key _ _))
+    = if key `elem` [SDLK_RIGHT, SDLK_LEFT, SDLK_DOWN, SDLK_UP] then return $ gd{pacman = plUpdateDir pacman dt dir}
+      else return gd where dir = case key of
+                                   SDLK_RIGHT -> E
+                                   SDLK_LEFT  -> W
+                                   SDLK_DOWN  -> S
+                                   SDLK_UP    -> N
+gdHandleEvent gd _ _ = return gd
+
+gdUpdate :: GameData -> TimeDelta -> IO(GameData)
+gdUpdate gameData dt
+    = do event <- pollEvent
+         (GameData balls pacman enemies board) <- gdHandleEvent gameData dt event
+         let pacman' = plMove pacman dt board
+         let collidesWithPlayer ball = 10 > vlen (ball `vsub` plGetPos pacman)
+         let (consumed, notConsumed) = List.partition collidesWithPlayer balls
+         return$ GameData notConsumed pacman' enemies board
+
+---- Main
 loop :: CpuTime -> GameData -> SurfacesMap -> IO ()
 loop startTime gameData images
     = do endTime <- getCPUTime
          let dt = (fromIntegral (endTime - startTime)) / (10^11)
-         gameData' <- update gameData dt
-         display gameData' images
+         gameData' <- gdUpdate gameData dt
+         gdDisplay gameData' images
          loop endTime gameData' images
-         where
-           update gameData dt = do event <- pollEvent
-                                   (GameData objects balls pacman enemies board) <- handleEvent dt event gameData
-                                   let objects' = moveObjects objects dt
-                                   let pacman' = movePacman dt pacman board
-                                   let collidesWithPlayer ball = 10 > vlen (ball `vsub` getPlPos pacman)
-                                   let (consumed, notConsumed) = List.partition collidesWithPlayer balls
-                                   return$ GameData objects' notConsumed pacman' enemies board
 
 assert :: Show a => Eq a => a -> a -> String -> IO()
 assert expected actual msg = if expected == actual then return () else error $ concat ["\nexpected: ", (show expected), "\nactual: ", (show actual), "\ndata: ", msg]
@@ -192,11 +199,10 @@ main = withInit [InitVideo] $
        enableUnicode True
        images <- loadImages
        startTime <- getCPUTime
-       loop startTime (GameData objects balls player enemies board1) images
-       where objects = [ (Vector 50 50), (Vector 350 150) ]
-             enemies = map makeEnemy [(Vector 500 250), (Vector 450 300)]
-             balls = [ Vector (float x * 50.0) (float y * 50.0) | x <- range (0, 12-1), y <- range(0, 7-1) ]
-             player = (Player (Vector 0 0) X)
+       loop startTime (GameData balls player enemies board1) images
+       where enemies = map makeEnemy [(Vector 500 250), (Vector 450 300)]
+             balls   = map makeBall [ Vector (float x * 50.0) (float y * 50.0) | x <- range (0, 12-1), y <- range(0, 7-1) ]
+             player  = makePlayer (Vector 0 0)
              t=1; r=2; b=4; l=8
              tl=t+l; tr=t+r; bl=b+l; br=b+r; lr=l+r; tb=t+b
 
